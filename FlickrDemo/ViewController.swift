3e8c4187be14ea2846997a69855ec65ceaced25e
howard <linhongyi73@gmail.com>
2021-02-28 21:15:23 +0800

    使用Cocoapods 建立範例

---
+++ FlickrDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrDemo
+//
+//  Created by 林紘毅 on 2021/2/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '13.0'
+
+pod 'Alamofire'
+
+target 'FlickrDemo' do
+end
+
+
+target 'FlickrDemoTests' do
+end
+
+
+target 'FlickrDemoUITests' do
+end
